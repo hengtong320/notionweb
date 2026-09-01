559e13e72343a83112f47238c69fee2341c53c83
hengtong320 <564712388@qq.com>
2026-09-01 13:15:50 +0800

    Use validated Adobe photorealistic assets for v4.0

--- picture-drop/tools/download_v40_realistic_blessings.sh
+++ picture-drop/tools/download_v40_realistic_blessings.sh
@@ -14,18 +14,21 @@ fetch_asset() {
     return
   fi
   echo "download $name"
-  curl -L --fail --retry 4 --retry-all-errors --connect-timeout 20 --max-time 240 \
+  curl -L --fail --silent --show-error --retry 4 --retry-all-errors \
+    --connect-timeout 20 --max-time 240 \
     -A 'Mozilla/5.0 Jigsaw-Drop-GitHub-Builder' \
+    -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
     -o "$target.tmp" "$url"
   mv "$target.tmp" "$target"
 }
 
-fetch_asset '01-lotus-sunrise.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:ec33d9f8-65f0-4bd4-8b26-03fa375b2526'
-fetch_asset '02-trumpet-flower.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:f957ace1-ed8e-495e-afc8-da8c56d5a1f7'
-fetch_asset '03-jujube-orchard.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:10a475db-a9a6-4bd2-b1f1-3fbbec34077e'
-fetch_asset '04-elegant-woman.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:eb364ddf-c8ae-4d26-89c6-3bf4060b3d7f'
-fetch_asset '05-blessing-vase.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:e1bfb40f-e36a-4c2b-ae02-2ac97e0e61fa'
-fetch_asset '06-pine-crane.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:76510980-67fd-4f57-a0b3-e6b9d554f089'
+# Adobe Firefly photorealistic, full-bleed, text-free 3:4 bases.
+fetch_asset '01-lotus-sunrise.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:1f37c267-2bc6-4818-b7c6-6e118c7faabd'
+fetch_asset '02-trumpet-flower.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:69826483-d1b6-44f1-8405-c6817648ffb4'
+fetch_asset '03-jujube-orchard.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:e160543e-0160-4370-abaa-fd579d21b98d'
+fetch_asset '04-elegant-woman.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:f8e9d1ed-1d8a-4f28-8b70-7a1843afe874'
+fetch_asset '05-blessing-vase.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:7680b8f9-0ffa-4117-b473-32e69351fe2f'
+fetch_asset '06-pine-crane.png' 'https://photoshop-api.adobe.io/v2/short-url/urn:aaid:ps:US:49e2a024-d18c-4d8c-98e8-9782f90d1a1e'
 
 python - <<'PY'
 from pathlib import Path
@@ -37,7 +40,7 @@ for p in files:
     data=p.read_bytes()
     assert data[:8]==b'\x89PNG\r\n\x1a\n', f'not png: {p}'
     width,height=struct.unpack('>II',data[16:24])
-    assert (width,height)==(1080,1440), (p,width,height)
+    assert (width,height)==(896,1152), (p,width,height)
     assert len(data)>100_000, (p,len(data))
     print(p.name,width,height,len(data))
 PY
